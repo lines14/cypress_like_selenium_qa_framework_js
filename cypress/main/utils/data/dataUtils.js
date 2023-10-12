cda6b8251ed6e52d9c1de7c23fd629c6c49f0609
lines14 <a.lipatov@a-i.kz>
2023-10-12 16:21:35 +0600

    update data utils

--- cypress/main/utils/data/dataUtils.js
+++ cypress/main/utils/data/dataUtils.js
@@ -1,9 +1,34 @@
+const moment = require('moment');
+const JSONMapper = require('./JSONMapper');
+const JSONLoader = require('./JSONLoader');
 const { parseStringPromise } = require('xml2js');
 
 class DataUtils {
-    async XMLToJSON(xml) {
+    static async XMLToJSON(xml) {
         return (await parseStringPromise(xml)).response;
+    }
+
+    static mapPolicyESBDToTWB(
+        getPolicyData, 
+        getContract_By_NumberData, 
+        getClientByIDData
+        ) {
+        const firstMappedPart = JSONMapper.mapValues(
+            getPolicyData, 
+            getContract_By_NumberData, 
+            JSONLoader.getContract_By_NumberToGetPolicyMapSchema
+        );
+        const secondMappedPart = JSONMapper.mapValues(
+            getPolicyData, 
+            getClientByIDData, 
+            JSONLoader.getClientByIDToGetPolicyMapSchema
+        );
+        const mappedData = { ...firstMappedPart, ...secondMappedPart };
+        mappedData.date_create = (moment(mappedData.date_create, JSONLoader.dictESBD.timeFormat))
+        .format(JSONLoader.dictTWB.timeFormat);
+        const rewritedData = JSONMapper.rewriteValues(mappedData, JSONLoader.dictTWB, JSONLoader.dictESBD);
+        return JSONMapper.unflattenJSON(rewritedData);
     }
 }
 
-module.exports = new DataUtils();
+module.exports = DataUtils;
