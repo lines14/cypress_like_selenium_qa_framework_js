30c29fab06b30faf3a819cc021aa5343dace7841
lines14 <a.lipatov@a-i.kz>
2023-12-04 20:35:28 +0600

    optimize locators

--- cypress/test/pageObjects/policyRequestFormMutualOGPO.js
+++ cypress/test/pageObjects/policyRequestFormMutualOGPO.js
@@ -44,26 +44,26 @@ class policyRequestFormMutualOGPO extends BaseForm {
     
     constructor(startDate) {
         super(new XPATH('//h3[contains(text(), "Оформление полиса")]'), 'OGPO policy request page');
-        this.#phoneBox = new Textbox(new XPATH('//label[contains(text(), "Номер телефона")]//parent::div[@class="form-item"]//following-sibling::input[@type="tel"]'), 'phone');
+        this.#phoneBox = new Textbox(new XPATH('//label[contains(text(), "Номер телефона")]//following-sibling::input[@type="tel"]'), 'phone');
         this.#nextButton = new Button(new XPATH('//button[contains(text(), "Далее")]'), 'next button');
-        this.#SMSCodeBox = new Textbox(new XPATH('//label[contains(text(), "SMS-код")]//parent::div[@class="form-item"]//following-sibling::input[@type="number"]'), 'SMS code');
+        this.#SMSCodeBox = new Textbox(new XPATH('//label[contains(text(), "SMS-код")]//following-sibling::input[@type="number"]'), 'SMS code');
         this.#IINBox = new Textbox(new XPATH('//input[@id="iinOgpo"]'), 'IIN');
         this.#selectedClientName = new Label(new XPATH('//span[@class="form-fio"]'), 'selected client name');
-        this.#displayedClientName = new Label(new XPATH('//span[contains(text(), "Водители:")]//following-sibling::div[@class="text-14"]//child::span'), 'displayed client name');
-        this.#emailBox = new Textbox(new XPATH('//label[contains(text(), "Email")]//parent::div[@class="form-item"]//following-sibling::input[@type="text"]'), 'email');
-        this.#addressBox = new Textbox(new XPATH('//label[contains(text(), "Адрес")]//parent::div[contains(@class, "form-item")]//following-sibling::input[@type="text"]'), 'address');
+        this.#displayedClientName = new Label(new XPATH('//span[contains(text(), "Водители:")]//following-sibling::div[@class="text-14"]//span'), 'displayed client name');
+        this.#emailBox = new Textbox(new XPATH('//label[contains(text(), "Email")]//following-sibling::input[@type="text"]'), 'email');
+        this.#addressBox = new Textbox(new XPATH('//label[contains(text(), "Адрес")]//following-sibling::input[@type="text"]'), 'address');
         this.#saveButton = new Button(new XPATH('//button[contains(text(), "Сохранить")]'), 'save button');
-        this.#carNumberBox = new Textbox(new XPATH('//label[contains(text(), "Гос. номер")]//parent::div[@class="form-item"]//following-sibling::input'), 'car number');
-        this.#carRegistrationBox = new Textbox(new XPATH('//label[contains(text(), "Номер техпаспорта")]//parent::div[@class="form-item"]//following-sibling::input'), 'car registration');
+        this.#carNumberBox = new Textbox(new XPATH('//label[contains(text(), "Гос. номер")]//following-sibling::input'), 'car number');
+        this.#carRegistrationBox = new Textbox(new XPATH('//label[contains(text(), "Номер техпаспорта")]//following-sibling::input'), 'car registration');
         this.#searchButton = new Button(new XPATH('//button[contains(text(), "Поиск")]'), 'search button');
-        this.#displayedCarNumber = new Label(new XPATH('//span[contains(text(), "Транспорт:")]//following-sibling::div[@class="text-14"]//child::span'), 'displayed car number');
+        this.#displayedCarNumber = new Label(new XPATH('//span[contains(text(), "Транспорт:")]//following-sibling::div[@class="text-14"]//span'), 'displayed car number');
         this.#selectedCarModel = new Label(new XPATH('//span[@class="subtitle-16 -orange"]'), 'selected car model');
         this.#selectedCarManufacturedYear = new Label(new XPATH('//span[@class="subtitle-16 -grey"]'), 'selected car manufactured year');
-        this.#calendarButton = new Button(new XPATH('//span[contains(text(), "Дата начала договора")]//parent::div[@class="form-item"]//following-sibling::div[@class="mx-datepicker"]'), 'calendar button');
+        this.#calendarButton = new Button(new XPATH('//span[contains(text(), "Дата начала договора")]//following-sibling::div[@class="mx-datepicker"]'), 'calendar button');
         this.#calendarRightArrowButton = new Button(new XPATH('//button[contains(@class, "mx-btn-icon-right")]//i'), 'right calendar arrow button');
         this.#startDateButton = new Button(new XPATH(`//td[@title="${startDate}"]`), 'start date');
         this.#selectedDate = new Button(new XPATH('//input[@name="date"]'), 'selected date');
-        this.#displayedDate = new Label(new XPATH('//span[contains(text(), "Дата начала:")]//following-sibling::div[@class="text-14"]//child::span'), 'displayed date');
+        this.#displayedDate = new Label(new XPATH('//span[contains(text(), "Дата начала:")]//following-sibling::div[@class="text-14"]//span'), 'displayed date');
         this.#moreLink = new Button(new XPATH('//span[contains(text(), "Еще")]'), 'more link');
         this.#SMSNotifyCheckboxLabel = new Label(new XPATH('//label[contains(text(), "Получить уведомление через СМС")]'), 'SMS notify checkbox');
         this.#calculateButton = new Button(new XPATH('//button[@class="button -green right dt"]'), 'calculate button');
--- cypress/test/pageObjects/policyRequestFormShanyrak.js
+++ cypress/test/pageObjects/policyRequestFormShanyrak.js
@@ -38,29 +38,29 @@ class PolicyRequestFormShanyrak extends BaseForm {
     constructor(startDate) {
         super(new XPATH('//h3[contains(text(), "Оформление полиса")]'), 'Shanyrak policy request page');
         this.#nextButton = new Button(new XPATH('//button[contains(text(), "Далее")]'), 'next button');
-        this.#phoneBox = new Textbox(new XPATH('//label[contains(text(), "Номер телефона")]//parent::div[@class="form-item"]//following-sibling::input[@type="tel"]'), 'phone');
-        this.#SMSCodeBox = new Textbox(new XPATH('//label[contains(text(), "SMS-код")]//parent::div[@class="form-item"]//following-sibling::input[@type="number"]'), 'SMS code');
+        this.#phoneBox = new Textbox(new XPATH('//label[contains(text(), "Номер телефона")]//following-sibling::input[@type="tel"]'), 'phone');
+        this.#SMSCodeBox = new Textbox(new XPATH('//label[contains(text(), "SMS-код")]//following-sibling::input[@type="number"]'), 'SMS code');
         this.#IINBox = new Textbox(new XPATH('//input[@id="iinHome"]'), 'IIN');
         this.#selectedClientName = new Label(new XPATH('//span[@class="subtitle-16 -green"]'), 'selected client name');
-        this.#emailBox = new Textbox(new XPATH('//label[contains(text(), "Email")]//parent::div[@class="form-item"]//following-sibling::input[@type="text"]'), 'email');
-        this.#citiesDropdownButton = new Button(new XPATH('//span[contains(text(), "Город")]//parent::div[@class="form-item"]//following-sibling::div[contains(@class, "multiselect")]//div[@class="multiselect__select"]'), 'cities dropdown');
+        this.#emailBox = new Textbox(new XPATH('//label[contains(text(), "Email")]//following-sibling::input[@type="text"]'), 'email');
+        this.#citiesDropdownButton = new Button(new XPATH('//span[contains(text(), "Город")]//following-sibling::div[contains(@class, "multiselect")]//div[@class="multiselect__select"]'), 'cities dropdown');
         this.#citiesDropdownElements = new Button(new XPATH('//span[contains(text(), "Город")]//parent::div[@class="form-item"]//following-sibling::div[@class="multiselect__content-wrapper"]//descendant::li[@class="multiselect__element"]//span[contains(@class, "multiselect__option")]//span'), 'cities dropdown elements');
-        this.#insuranceObjectAddressStreetBox = new Textbox(new XPATH('//label[contains(text(), "Улица / проспект / микрорайон")]//parent::div[@class="form-item"]//following-sibling::input'), 'insurance object address street');
+        this.#insuranceObjectAddressStreetBox = new Textbox(new XPATH('//label[contains(text(), "Улица / проспект / микрорайон")]//following-sibling::input'), 'insurance object address street');
         this.#privateHomeCheckboxWrapper = new Label(new XPATH('//label[contains(text(), "Частный дом")]//parent::div[contains(@class, "item")]'), 'private home checkbox wrapper');
         this.#privateHomeCheckboxLabel = new Label(new XPATH('//label[contains(text(), "Частный дом")]'), 'private home checkbox');
-        this.#insuranceObjectAddressHouseNumberBox = new Textbox(new XPATH('//label[contains(text(), "Дом")]//parent::div[@class="form-item"]//following-sibling::input'), 'insurance object address house number');
-        this.#insuranceObjectAddressApartmentNumberBox = new Textbox(new XPATH('//label[contains(text(), "Квартира")]//parent::div[@class="form-item"]//following-sibling::input'), 'insurance object address apartment number');
+        this.#insuranceObjectAddressHouseNumberBox = new Textbox(new XPATH('//label[contains(text(), "Дом")]//following-sibling::input'), 'insurance object address house number');
+        this.#insuranceObjectAddressApartmentNumberBox = new Textbox(new XPATH('//label[contains(text(), "Квартира")]//following-sibling::input'), 'insurance object address apartment number');
         this.#confirmationCheckbox = new Checkbox(new XPATH('//input[@type="checkbox" and @id="check2"]'), 'confirmation checkbox');
         this.#startDateButton = new Button(new XPATH(`//td[@title="${startDate}"]`), 'start date');
         this.#calendarRightArrowButton = new Button(new XPATH('//button[contains(@class, "mx-btn-icon-right")]//i'), 'right calendar arrow button');
-        this.#calendarButton = new Button(new XPATH('//span[contains(text(), "Дата начала договора")]//parent::div[@class="form-item"]//following-sibling::div[@class="form-item__icon"]'), 'calendar button');
+        this.#calendarButton = new Button(new XPATH('//span[contains(text(), "Дата начала договора")]//following-sibling::div[@class="form-item__icon"]'), 'calendar button');
         this.#saveButton = new Button(new XPATH('//button[contains(text(), "Сохранить")]'), 'save button');
         this.#acceptanceCheckbox = new Checkbox(new XPATH('//input[@type="checkbox" and @id="familiarized"]'), 'acceptance checkbox');
         this.#kaspiPayButton = new Button(new XPATH('//button[contains(@class, "-red")]'), 'Kaspi pay button');
         this.#orderPayment = new Label(new XPATH('//p[contains(text(), "Оплата заказа")]'), 'order payment');
         this.#price = new Label(new XPATH('//div[@class="text" and contains(normalize-space(), "Фиксированная стоимость")]//preceding-sibling::div[@class="price"]'), 'price');
         this.#sumToPay = new Label(new XPATH('//h6[contains(text(), "Общая сумма")]//following-sibling::h6[contains(text(), "₸")]'), 'sum to pay');
-        this.#paymentNumber = new Label(new XPATH('//div[contains(text(), "номер оплаты на Kaspi")]//child::b'), 'payment number');
+        this.#paymentNumber = new Label(new XPATH('//div[contains(text(), "номер оплаты на Kaspi")]//b'), 'payment number');
         this.#epayButton = new Button(new XPATH('//button[contains(text(), "картой")]'), 'Epay button');
         this.#mainPageButton = new Button(new XPATH('//a[contains(text(), "На главную")]'), 'main page button');
     }
