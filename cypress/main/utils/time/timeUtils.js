59cbb24cb8858ecd694b157017b19371f14b0f6c
lines14 <a.lipatov@a-i.kz>
2024-03-20 20:15:22 +0500

    update

--- cypress/main/utils/time/timeUtils.js
+++ cypress/main/utils/time/timeUtils.js
@@ -2,12 +2,13 @@ const moment = require('moment');
 const JSONLoader = require('../data/JSONLoader');
 
 class TimeUtils {
-    static getIntervalFromTomorrow(count, unitOfTime) {
-        const startDate = moment().add(1, 'days')
-        .format(JSONLoader.testData.datesFormat);
-        const finishDate = moment().add(count, unitOfTime)
-        .format(JSONLoader.testData.datesFormat);
-        return { startDate: startDate, finishDate: finishDate }
+    static getDatesInterval(count, unitOfTime, dateBegin) {
+        const startDate = moment().add(1, 'days').format(JSONLoader.testData.datesFormat);
+        const finishDate = dateBegin 
+        ? moment(dateBegin, JSONLoader.testData.datesFormat).add(count, unitOfTime)
+        .subtract(1, 'days').format(JSONLoader.testData.datesFormat) 
+        : moment().add(count, unitOfTime).format(JSONLoader.testData.datesFormat);
+        return { startDate: dateBegin ?? startDate, finishDate }
     }
 }
 
