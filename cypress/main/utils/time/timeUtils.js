ff882ef476c0cc5a28869546a1bc41dfbb4ad6f4
lines14 <a.lipatov@a-i.kz>
2024-05-14 22:49:22 +0500

    up

--- cypress/main/utils/time/timeUtils.js
+++ cypress/main/utils/time/timeUtils.js
@@ -3,32 +3,31 @@ const JSONLoader = require('../data/JSONLoader');
 
 class TimeUtils {
   static getDatesInterval(count, unitOfTime, options = {}) {
-    const dateBegin = options.dateBegin || null;
-    const isIncluded = options.isIncluded || true;
-    const startNextDay = options.startNextDay || true;
-    const reverseInterval = options.reverseInterval || false;
+    const dateBegin = options.dateBegin ?? null;
+    const isIncluded = options.isIncluded ?? true;
+    const startNextDay = options.startNextDay ?? true;
+    const reverseInterval = options.reverseInterval ?? false;
 
-    const startDate = startNextDay
-      ? moment().add(1, 'days').format(JSONLoader.testData.datesFormat)
-      : moment().format(JSONLoader.testData.datesFormat);
+    let startDate = startNextDay ? moment().add(1, 'days') : moment();
 
     let finishDate;
     if (reverseInterval) {
       finishDate = dateBegin
         ? moment(dateBegin, JSONLoader.testData.datesFormat).subtract(count, unitOfTime)
-        : moment().subtract(count, unitOfTime);
+        : startDate.subtract(count, unitOfTime);
       if (isIncluded) {
         finishDate = finishDate.add(1, 'days');
       }
     } else {
       finishDate = dateBegin
         ? moment(dateBegin, JSONLoader.testData.datesFormat).add(count, unitOfTime)
-        : moment().add(count, unitOfTime);
+        : startDate.add(count, unitOfTime);
       if (isIncluded) {
         finishDate = finishDate.subtract(1, 'days');
       }
     }
 
+    startDate = startDate.format(JSONLoader.testData.datesFormat);
     finishDate = finishDate.format(JSONLoader.testData.datesFormat);
     return { startDate: dateBegin ?? startDate, finishDate };
   }
