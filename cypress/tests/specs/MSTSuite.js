2e714f9db3e88a81d492d8aa0ac8b1b14f2c3ef5
lines14 <a.lipatov@a-i.kz>
2024-06-07 18:25:54 +0500

    up

--- cypress/tests/specs/MSTSuite.js
+++ cypress/tests/specs/MSTSuite.js
@@ -1,10 +1,9 @@
-const { ePay } = require('./ePay');
 const { kaspiPay } = require('./kaspiPay');
 const { userPathMST } = require('./userPathMST');
 const Randomizer = require('../../main/utils/random/randomizer');
 
 describe('MST test suite:', () => {
-  const paymentVariants = [kaspiPay, ePay];
+  const paymentVariants = [kaspiPay];
   userPathMST();
   paymentVariants[Randomizer.getRandomInteger(paymentVariants.length - 1)]();
 });
--- cypress/tests/specs/MutualOGPOSuite.js
+++ cypress/tests/specs/MutualOGPOSuite.js
@@ -1,10 +1,9 @@
-const { ePay } = require('./ePay');
 const { kaspiPay } = require('./kaspiPay');
 const { userPathMutualOGPO } = require('./userPathMutualOGPO');
 const Randomizer = require('../../main/utils/random/randomizer');
 
 describe('OGPO & Mutual test suite:', () => {
-  const paymentVariants = [kaspiPay, ePay];
+  const paymentVariants = [kaspiPay];
   userPathMutualOGPO();
   paymentVariants[Randomizer.getRandomInteger(paymentVariants.length - 1)]();
 });
